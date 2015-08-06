2b8b7fd97ee32b1218200781ae6376e56d86be56
levi shuster <levi.shusterman@gmail.com>
2015-08-06 16:38:16 +0300

    Writing a test

--- Organize_Data/analyze_parameters.py
+++ Organize_Data/analyze_parameters.py
@@ -1,28 +1,47 @@
 
-def analyze_parameters(**kwargs):
-    #iterate through kwargs and
-    grader = ParamMethods()
-
-    for key, value in kwargs.iteritems():
-        param_to_grade = grader.assign_function(key)
-
+# Class that is meant to evaluate parameters and return a grade
+# for two song data that are passed to it
 class ParamMethods(object):
 
     def __init__(self, genre = 1, key = 1, bpm = 1):
+        # we can override these later based on our research
+        # these constructor variables must add up to a whole number
+        # that is equal to how many they are
         self.genre_weight = genre
         self.key_weight = key
         self.bpm_weight = bpm
 
-    def assign_function(self, key_from_dict, value):
+    # Returns a grade for a transition betweeen two songs
+    def grade_parameters( self, first_song, second_song):
+    # iterate through kwargs and give a grade based on each parameter, take the average
+        grade = 0.0
+        num_of_params = 0.0
+
+        # compare two songs
+        for (key, value), (key2, value2) in first_song.iteritems(), second_song.iteritems():
+            if key == key2:
+                # analyze the parameter
+                param_func_grade = self.assign_function(key) # which parameter to analyze?
+                grade += param_func_grade(value, value2) # grade the overlap, of two things, such as bpm
+            else:
+                print "%s != %s ; incorrect keys received\n" % (key, key2)
+            num_of_params += 1
+
+        return grade / num_of_params # return the average
+
+    # assign a function to grade two parameters based on the name of the dict key
+    # this is meant to allow us to change the parameters that we use flexibly
+    def _assign_function(self, key_from_dict):
         if key_from_dict in 'grade_genre':
-            return grade_genre
-        pass
+            return self._grade_genre
+        elif key_from_dict in 'grade_key':
+            return self._grade_key
+        elif key_from_dict in 'grade_bpm':
+            return self._grade_bpm
 
-    def grade_genre(self):
-        pass
-
-    def grade_key(self):
-        pass
-
-    def grade_bpm(self):
-        pass
+    def _grade_genre(self):
+        return 1
+    def _grade_key(self):
+        return 1
+    def _grade_bpm(self):
+        return 1
--- Organize_Data/compile_matrices.py
+++ Organize_Data/compile_matrices.py
@@ -1,24 +1,25 @@
 # Levi Shusterman
 # Israel Tech Challenge Hackathon
 
-import data_from_audio_api # Adam's API
+import data_from_audio_api_mock # Adam's API
 import analyze_parameters
 
 class CompileMatrices(self):
 
     def __init__(self):
-        self.all_song_data = data_from_audio_api.get_song_stats()
-        self.num_of_songs = self.data_from_audo_api.get_num_of_songs() # gets number of songs from first member of list
+        self.all_song_data = data_from_audio_api_mock.get_song_stats() # a list of dicts
+        self.num_of_songs = self.data_from_audio_api_mock.get_num_of_songs() # gets number of songs from first member of list
 
         # puts matrices together
         self.compile_matrices()
 
     def compile_matrices(self):
-        # self.overall_matrix = self.compile_overall_matrix()
-        # self.transtion_matrix = self.compile_transition_matrix()
+
+        from analyze_parameters import ParamMethods
+
         size = self.num_of_songs
         self.overall_matrix = [[0 for x in range(size)] for x in range(size)]
-
+        grader = analyze_parameters.ParamMethods()
         x = 0
 
         while x < size:
@@ -26,16 +27,8 @@ class CompileMatrices(self):
             while y <= size:
 
                 self.overall_matrix[x][y] = \
-                analyze_parameters.overall_analyze_parameters( self.all_song_data[x]['middle'],
+                ParamMethods.grade_parameters( self.all_song_data[x]['middle'],
                                     self.all_song_data[y]['middle'] )
-
                 y = y+1
             x = x+1
-
-    def compile_overall_matrix(self):
-
-
-    def compile_transition_matrix(self):
-
-
 
