1540c1301a842d881b933c02c85cc9c2b051877a
levi shuster <levi.shusterman@gmail.com>
2015-08-06 15:16:21 +0300

    Building analyze_parameters

--- Organize_Data/analyze_parameters.py
+++ Organize_Data/analyze_parameters.py
@@ -1,2 +1,28 @@
 
-def analyze_parameters()
+def analyze_parameters(**kwargs):
+    #iterate through kwargs and
+    grader = ParamMethods()
+
+    for key, value in kwargs.iteritems():
+        param_to_grade = grader.assign_function(key)
+
+class ParamMethods(object):
+
+    def __init__(self, genre = 1, key = 1, bpm = 1):
+        self.genre_weight = genre
+        self.key_weight = key
+        self.bpm_weight = bpm
+
+    def assign_function(self, key_from_dict, value):
+        if key_from_dict in 'grade_genre':
+            return grade_genre
+        pass
+
+    def grade_genre(self):
+        pass
+
+    def grade_key(self):
+        pass
+
+    def grade_bpm(self):
+        pass
